63ad4a1c43380ecd12256e95b7ad122db77bc250
nicknessii <545310317@qq.com>
2020-01-22 17:12:35 +0800

    使用策略模式对打折系统进行重构

--- src/main/java/com/github/hcsp/polymorphism/OnlyVipDiscountStrategy.java
+++ src/main/java/com/github/hcsp/polymorphism/OnlyVipDiscountStrategy.java
@@ -3,7 +3,7 @@ package com.github.hcsp.polymorphism;
 public class OnlyVipDiscountStrategy extends DiscountStrategy{
     @Override
     public int discount(int price,User user){
-       if(user.isVIP()){
+       if(user.isVip()){
            return (int)(price*0.95);
                   }else {
            return price;
