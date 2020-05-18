16226e7deeff60f56793f7cb1fcefacb676e495d
Administrator <756433556@qq.com>
2020-05-19 00:11:12 +0800

    使用策略模式

--- src/main/java/com/github/hcsp/polymorphism/NoDiscountStrategy.java
+++ src/main/java/com/github/hcsp/polymorphism/NoDiscountStrategy.java
@@ -3,6 +3,6 @@ package com.github.hcsp.polymorphism;
 public class NoDiscountStrategy extends DiscountStrategy{
     @Override
     public int discount(int price, User user) {
-        return super.discount(price, user);
+        return price;
     }
 }
