df76b7c762137969646cad70350a2277c90c6e16
LiDianguang <1789681874@qq.com>
2022-03-28 02:58:15 +0800

    Update

--- src/main/java/com/github/hcsp/polymorphism/Discount95Strategy.java
+++ src/main/java/com/github/hcsp/polymorphism/Discount95Strategy.java
@@ -2,7 +2,7 @@ package com.github.hcsp.polymorphism;
 
 public class Discount95Strategy extends DiscountStrategy {
     @Override
-    public int discount(int price, User user){
+    public int discount(int price, User user) {
         return (int) (price * 0.95);
     }
 }
--- src/main/java/com/github/hcsp/polymorphism/NoDiscountStrategy.java
+++ src/main/java/com/github/hcsp/polymorphism/NoDiscountStrategy.java
@@ -2,7 +2,7 @@ package com.github.hcsp.polymorphism;
 
 public class NoDiscountStrategy extends DiscountStrategy {
     @Override
-    public int discount(int price, User user){
-        return price ;
+    public int discount(int price, User user) {
+        return price;
     }
 }
